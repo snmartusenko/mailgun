617fc67f190f4a42c2bb11bd7a7d22a667145ebd
Sergey_M <snmartusenko@gmail.com>
2017-10-13 02:01:26 +0300

    added auth; moved user model

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            'model' => App\models\user\User::class,
         ],
 
         // 'users' => [
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,7 +12,7 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+$factory->define(App\models\user\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
