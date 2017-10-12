b4c7e55e5ce10d2b51442e7502936c64c94f97ac
Sergey_M <snmartusenko@gmail.com>
2017-10-13 02:26:03 +0300

    added campaign migration, model, controller

---
+++ app/models/campaign/Campaign.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\models\campaign;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Campaign extends Model
+{
+    use SoftDeletes;
+
+    //
+}
