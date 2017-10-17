5ddd5635ca315e28d05e6f1bbf84021b07e40c1d
Sergey_M <snmartusenko@gmail.com>
2017-10-17 11:48:40 +0300

    created template views

---
+++ resources/views/template/_form.blade.php
@@ -0,0 +1,7 @@
+<div class="form-group">
+    {!!Form::label('name', 'Name') !!}
+    {!!Form::text('name', null, ['class' => 'form-control']) !!}
+
+    {!!Form::label('content', 'Content') !!}
+    {!!Form::text('content', null, ['class' => 'form-control']) !!}
+</div>
---
+++ resources/views/template/create.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.panel')
+
+<?php  /** @var \Illuminate\Support\ViewErrorBag $errors */  ?>
+
+@section('panel')
+    <div class="panel-heading container-fluid">
+        <div class="form-group">
+            <div class="centered-child col-md-11 col-sm-10 col-xs-10"><b>New Template</b></div>
+        </div>
+    </div>
+
+    <div class="panel-body">
+        {!! Form::open(['route' => 'template.store']) !!}
+
+        @include('template._form')
+
+        <div class="form-group">
+            {!! Form::button('Create', ['type' => 'submit', 'class' => 'btn btn-primary']) !!}
+        </div>
+
+        {!! Form::close() !!}
+
+    </div>
+
+@endsection
