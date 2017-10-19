691a57995a5a32e322f447ae25ce3c86ee1db502
Sergey_M <snmartusenko@gmail.com>
2017-10-19 11:15:59 +0300

    added bunch views

---
+++ resources/views/bunch/_form.blade.php
@@ -0,0 +1,7 @@
+<div class="form-group">
+    {!!Form::label('name', 'Name') !!}
+    {!!Form::text('name', null, ['class' => 'form-control']) !!}
+
+    {!!Form::label('description', 'Description') !!}
+    {!!Form::text('description', null, ['class' => 'form-control']) !!}
+</div>
---
+++ resources/views/bunch/show.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.panel')
+
+<?php
+/** @var \Illuminate\Support\ViewErrorBag $errors */
+/** @var \App\models\bunch\Bunch $bunch */
+?>
+
+@section('panel')
+
+    <div class="panel-heading container-fluid">
+        <div class="form-group">
+            <a class="btn btn-info btn-xs col-md-1 col-sm-2 col-xs-2" href="{{route('bunch.index')}}">
+                <i class="fa fa-backward" aria-hidden="true"></i> back
+            </a>
+            <div class="centered-child col-md-9 col-sm-7 col-xs-6">bunch: <b>{{$bunch->name}}</b></div>
+            <div class="col-md-2 col-sm-3 col-xs-4">
+                <div class="pull-right">
+                    {{Form::open(['class' => 'confirm-delete', 'route' => ['bunch.destroy', $bunch->id], 'method' => 'DELETE'])}}
+                    {{ link_to_route('bunch.edit', 'edit', [$bunch->id], ['class' => 'btn btn-primary btn-xs']) }} |
+                    {{Form::button('Delete', ['class' => 'btn btn-danger btn-xs', 'type' => 'submit'])}}
+                    {{Form::close()}}
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="panel-body">
+
+        <table class="table table-bordered table-responsive">
+            <tr>
+                <th width="25%">Attribute</th>
+                <th width="75%">Value</th>
+            </tr>
+            @foreach ($bunch->getAttributes() as $attribute => $value)
+                <tr>
+                    <td>{{$attribute}}</td>
+                    <td>{{$value}}</td>
+                </tr>
+            @endforeach
+        </table>
+
+    </div>
+
+@endsection
