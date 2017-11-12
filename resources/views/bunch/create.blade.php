1e640faa917d942da75825b7642bfe361b70cc5b
Sergey_M <snmartusenko@gmail.com>
2017-11-12 20:24:03 +0200

    added a errors output

--- resources/views/bunch/create.blade.php
+++ resources/views/bunch/create.blade.php
@@ -3,6 +3,17 @@
 <?php  /** @var \Illuminate\Support\ViewErrorBag $errors */  ?>
 
 @section('panel')
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <div class="panel-heading container-fluid">
         <div class="form-group">
             <div class="centered-child col-md-11 col-sm-10 col-xs-10"><b>New Bunch</b></div>
--- resources/views/subscriber/edit.blade.php
+++ resources/views/subscriber/edit.blade.php
@@ -4,6 +4,16 @@
 
 @section('panel')
 
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <div class="panel-heading container-fluid">
         <div class="form-group">
             <a class="btn btn-info btn-xs col-md-1 col-sm-2 col-xs-2" href="{{route('bunch.subscriber.index', $bunch)}}">
