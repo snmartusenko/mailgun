0e6e28730a84b686e3f9b8a8e0d5ed0963f82bce
Sergey_M <snmartusenko@gmail.com>
2017-11-21 00:33:25 +0200

    created view after sending

---
+++ resources/views/campaign/sent.blade.php
@@ -0,0 +1,54 @@
+<?php
+use App\models\template\Template;
+use App\models\bunch\Bunch;
+
+$models = $campaign->bunch->subscribers;
+?>
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+
+            <a class="btn btn-info btn-xs col-md-1 col-sm-2 col-xs-2" href="{{route('campaign.index')}}">
+                <i class="fa fa-backward" aria-hidden="true"></i> back
+            </a>
+
+            <div class="col-md-8 col-md-offset-2">
+
+
+                <div class="panel panel-default">
+                    <div class="panel-heading">The next Subscribers will receive the message:</div>
+
+                    <div class="panel-body">
+
+                        <table class="table table-bordered table-responsive table-striped">
+                            <tr>
+                                <th width="25%">Firstname</th>
+                                <th width="30%">Lastname</th>
+                                <th width="45%">Email</th>
+                            </tr>
+                            <tr>
+                                <td colspan="3" class="light-green-background no-padding" title="Create new campaign">
+                                    <div class="row centered-child">
+                                        <div class="col-md-12">
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+
+                            @foreach ($models as $model)
+                                <tr>
+                                    <td>{{$model->firstname}}</td>
+                                    <td>{{$model->lastname}}</td>
+                                    <td>{{$model->email}}</td>
+                                </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
